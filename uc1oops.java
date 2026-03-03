4af1d6e2ba4ac9ebea34a4af266108b9fad8c6c4
praneeswarreddy <sathi.praneeswarreddy03@gmail.com>
2026-03-03 22:10:17 +0530

    Add files via upload

---
+++ uc1oops.java
@@ -0,0 +1,14 @@
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * 
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Print OOPS to console
+        System.out.println("OOPS");
+    }
+}
